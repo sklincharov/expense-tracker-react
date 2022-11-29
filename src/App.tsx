77e93f564b0523435d868a2a90e162711d6dd39e
sklincharov <klincharov.slavko@gmail.com>
2022-11-29 15:26:01 -0600

    added placeholder components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import Expense from './components/Expense';
 import Header from './components/Header';
+import TransactionList from './components/TransactionList';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <div className="container">
+        <Balance />
+        <Expense />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
